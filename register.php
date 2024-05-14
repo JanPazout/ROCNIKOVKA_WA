4700eb192a1312329e9a52086b530157321b9b6f
Pesíno <pazout@spsejecna.cz>
2024-05-14 18:08:30 +0200

    register, loggin, css, bez funkcnosti(skriptu). Druhy commit

---
+++ register.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/forms.css">
+    <title>Registrace</title>
+</head>
+<body>
+<?php include 'navbar.php'?>
+
+
+<div class=" d-flex container justify-content-center align-items-center"  style="height: 90vh">
+
+    <form action="./scripts/registerscript.php" method="POST">
+        <h2>Registrace</h2>
+        <div class="form-group">
+            <label for="username">Uživatelské jméno:</label>
+            <input type="text" class="form-control" id="username" name="username" required>
+        </div>
+        <div class="form-group email">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Heslo:</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <button type="submit" class="btn ">Registrovat</button>
+        <div class="container d-flex justify-content-center" style="margin-top: 10px">
+             <a href="login.php">Přihlásit</a>
+        </div>
+    </form>
+
+</div>
+
+<?php include 'footer.php'?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
